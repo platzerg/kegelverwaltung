e0d0c090f07ff15c69e4d8945759a15fda6e37c1
guenter.platzerworld@gmail.com <guenter.platzerworld@gmail.com@b8e675bf-9cac-a00e-0ea5-716864ca4e80>
2011-03-17 10:33:32 +0000

    WebView

--- trunk/Kegelverwaltung/src/platzerworld/kegeln/gui/SchnittlisteAnzeigen.java
+++ trunk/Kegelverwaltung/src/platzerworld/kegeln/gui/SchnittlisteAnzeigen.java
@@ -1,23 +1,10 @@
 package platzerworld.kegeln.gui;
 
 import platzerworld.kegeln.R;
-import platzerworld.kegeln.common.db.KlassenFilterQueryProvider;
-import platzerworld.kegeln.common.db.PerformanterListenAdapter;
-import platzerworld.kegeln.common.db.PerformanterSpinnerAdapter;
-import platzerworld.kegeln.klasse.db.KlasseSpeicher;
-import platzerworld.kegeln.mannschaft.db.MannschaftSpeicher;
-import platzerworld.kegeln.spieler.db.SpielerTbl;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ListActivity;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Spinner;
+import android.webkit.WebView;
 
 /**
  * Zeigt die Liste der Geokontakte an.
@@ -28,19 +15,22 @@ import android.widget.Spinner;
 public class SchnittlisteAnzeigen extends Activity {
 
 	/** Kuerzel fuers Logging. */
-	private static final String TAG = ErgebnisseAnzeigen.class.getSimpleName();
+	private static final String TAG = SchnittlisteAnzeigen.class.getSimpleName();
 
-	private MannschaftSpeicher mMannschaftSpeicher;
 
 	@Override
 	protected void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		Log.d(TAG, "onCreate(): entered...");
 
-		// start tracing to "/sdcard/listview.trace"
-		// Debug.startMethodTracing("listview");
-
 		setContentView(R.layout.schnittliste_anzeigen);
+        
+        WebView webView= (WebView) findViewById(R.id.wv_schnittliste_anzeigen);
+        
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.loadUrl("http://www.kegelkreisrunde.de/punktspielbetrieb/schnittwertung/index.html");	
+		
+
 
 
 	}
